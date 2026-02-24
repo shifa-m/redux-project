e58a1689ad689283a0dfd5ee5b65589f8a872d60
shifa-m <shifamahreen786@gmail.com>
2026-02-24 23:16:14 +0530

    usestate

--- src/component/ResultGrid.jsx
+++ src/component/ResultGrid.jsx
@@ -1,29 +1,58 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {fetchPhotos,fetchVideos} from '../api/mediaApi'
 import {setQuery,setLoading,setError,setResults} from '../redux/features/searchSlice'
 import { useEffect } from 'react'
 
 const ResultGrid = () => {
 
-    const {query,activeTab,results,loading,console,error}  =   
+    const dispatch=useDispatch()
+    const {query,activeTab,results,loading,error}  =   
     useSelector((store)=>store.search) 
     
-   const getData =async () =>{
+   
+  useEffect(function(){
+
+    const getData =async () =>{
     
-    let data
+    try {
+      dispatch(setLoading())
+      let data=[]
     if(activeTab == 'photos'){
-      data=await fetchPhotos(query)
+      let response=await fetchPhotos(query)
+      data=response.results.map((item)=>({
+   id :item.id,
+   type:'photo',
+   title:item.alt.description,
+   thumbnail:item.urls.small,
+   src:item.urls.full
+      }))
 
     }
     if(activeTab == 'videos'){
-      data=await fetchVideos(query)
+      let response=await fetchVideos(query)
+      data=response.videos.map((item)=>({
+        id:item.id,
+        type:'video',
+        title:item.user||'video',
+        thumbnail:item.image,
+        src:item.video_files[0].link
+      }
+
+      ))
     }
+    dispatch(setResults(data))
     }
-   
+    catch (err) {
+      dispatch(setError(err))
+      
+    }
+  }
+    getData()
+  },[query,activeTab])
+
 
   return (
     <div>
-      <button  onClick ={getData}>Get Data</button>
     </div>
   )
 }
